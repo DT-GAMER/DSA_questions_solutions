3bc7cfbd558cdcacfc58383e09b866e5d1a6f513
Abakpa Dominic <abakpad82@gmail.com>
2024-04-11 21:38:08 +0100

    Update Mongoose.ts

--- Mongoose.ts
+++ Mongoose.ts
@@ -2,22 +2,6 @@ import mongoose from 'mongoose';
 import { handleValidationErrors } from './validation';
 
 // Define your schema
-const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, 'First name is required'],
-        maxLength: [15, 'First name should be less than 15 characters']
-    },
-    lastName: {
-        type: String,
-        required: [true, 'Last name is required']
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: [true, 'User with email already exists']
-    }
-});
 
 // Apply custom error handling middleware
 UserSchema.post('validate', function(error: mongoose.Error.ValidationError, doc: mongoose.Document, next: mongoose.HookNextFunction) {
@@ -25,12 +9,7 @@ UserSchema.post('validate', function(error: mongoose.Error.ValidationError, doc:
     next(); // Call next middleware
 });
 
-// Create and export the model
-interface IUserModel extends mongoose.Document {
-    firstName: string;
-    lastName: string;
-    email: string;
-}
+// Define IUserModel
 
 export const UserModel = mongoose.model<IUserModel>('User', UserSchema);
 ￼Enter
